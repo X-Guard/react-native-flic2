209de40c97f61be213be3c8c72a7900e813a912a
Jesse Degger <degger@x-guard.nl>
2020-01-08 14:14:09 +0100

    Fix Flic2 reference

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import EventEmitter from 'react-native/Libraries/vendor/emitter/EventEmitter';
 import Flic2Button from './flic2Button.js';
 
 // we only care about flic 2 here
-const { Flic2 } = NativeModules;
+const Flic2Module = NativeModules.Flic2;
 
 /**
  * React Native Flic 2
